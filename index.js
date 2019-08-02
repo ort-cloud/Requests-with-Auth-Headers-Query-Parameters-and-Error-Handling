160ab82c6c75ea1c7e17a48769730d83f55b7bb8
Carl Ortutay <carlorty@gmail.com>
2019-08-01 21:12:48 -0400

    updates to js file

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//https://api.github.com/users/username/repos
+//
+
+const apiKey = "";
+const searchUrl = "https://api.github.com/users/username/repos";
+
+function formatQueryParams(userObject){
+  const items = Object.keys(userObject).map(key => `${key}=${userObject[key]}`).join("&");
+}
 
 function main(){
   $(`#submit`).on("click", function(){
@@ -8,6 +15,12 @@ function main(){
   })
 }
 
-function getUserName(username)
+function getUserName(username, sort="created"){
+  const userObject = {
+    type: "owner",
+    sort,
+    direction: "descending",
+  };
+}
 
 $(main);
