cd1952584d0b8863721b48126c85f7817ae50508
Carl Ortutay <carlorty@gmail.com>
2019-08-01 23:27:57 -0400

    completed js file after debbuging

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-//https://api.github.com/users/kristopherw2/repos?type=string&sort=string&direction=string
-
-let userQuery = $('#text-box').val();
 const apiKey = "";
-const searchUrl = `https://api.github.com/users/${userQuery}/repos`;
 
-function formatQueryParams(userObject){
-  const items = Object.keys(userObject).map(key => `${key}=${userObject[key]}`).join("&");
+function formatQueryParams(userObject) {
+  const items = Object.keys(userObject).map(key => `${key}=${userObject[key]}`)
+  return items.join("&");
 }
 
-const queryString = formatQueryParams(userQuery);
-const url = searchUrl + '?' + queryString;
-console.log(url);
-
-function main(){
-  $(`fieldset`).on("click", "#submit", function(){
+function main() {
+  $(`fieldset`).on("click", "#submit", function () {
     event.preventDefault();
     let userQuery = $('#text-box').val();
-    getUserName(userQuery, sort);
+    getUserName(userQuery);
   });
 }
 
-function getUserName(username, sort="created"){
-  const userObject = {
-    type: "owner",
-    sort,
-    direction: "descending",
-  };
+function getUserName(username) {
+  const searchUrl = `https://api.github.com/users/${username}/repos`;
+
+  fetch(searchUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+
 }
 
-fetch(url)
-.then(response => {
-  if (response.ok) {
-    return response.json();
-  }
-  throw new Error(response.statusText);
-})
-.then(responseJson => displayResults(responseJson))
-.catch(err => {
-  $('#js-error-message').text(`Something went wrong: ${err.message}`);
-});
+const displayResults = (responseJson) => {
+    $('.search-results').empty();
+  responseJson.forEach((repoObj) => {
+    $('.search-results').append(`
+          <div>${repoObj.name}</div>  
+          <div>${repoObj.html_url}</div>
+       `)
+  })
+
+}
 
 
 $(main);
