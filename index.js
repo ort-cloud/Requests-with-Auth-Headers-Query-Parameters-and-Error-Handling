dd65ebfa812cbfbfff48ad0d17245c3eb6a1cfed
Carl Ortutay <carlorty@gmail.com>
2019-08-01 20:31:53 -0400

    added html and js files. Made changes to both files

---
+++ index.js
@@ -0,0 +1,17 @@
+//https://api.github.com/users/username/repos
+
+function main(){
+
+}
+
+function submiter(){
+  $(`#submit`).on("click", function(){
+    event.preventDefault();
+    let userQuery = $('#text-box').val();
+    
+  })
+}
+
+function getUserName(username)
+
+$(main);
